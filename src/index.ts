1198d95a0c4a46a538cf077aff19d87f48dfd23e
InkoHX <inkohx@gmail.com>
2019-12-13 01:37:27 +0900

    DisplayOptions

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,48 @@ export type LogType = 'INFO'
   | 'ERROR'
   | 'DEBUG'
 
+export interface DisplayOptions {
+  info?: boolean
+  warn?: boolean
+  error?: boolean
+  debug?: boolean
+}
+
 export interface LoggerOptions {
   logging?: boolean
   file?: string
   timestamp?: string
+  display?: {
+    console?: DisplayOptions
+    file?: DisplayOptions
+  }
+}
+
+interface WriteOptions {
+  console?: boolean
+  file?: boolean
 }
 
 const defaultLoggerOptions: LoggerOptions = {
-  logging: false
+  logging: false,
+  timestamp: 'YYYY/MM/DD HH:mm:ss',
+  display: {
+    console: {
+      info: true,
+      warn: true,
+      error: true,
+      debug: true
+    },
+    file: {
+      info: true,
+      warn: true,
+      error: true,
+      debug: true
+    }
+  }
 }
 
-const colors: {[key in LogType]: (timestamp: string, data: string, noColor?: boolean) => string} = {
+const colors: { [key in LogType]: (timestamp: string, data: string, noColor?: boolean) => string } = {
   INFO: (timestamp, data) => `${chalk.bgBlueBright(`[${moment().format(timestamp)}]`)} ${data}`,
   WARN: (timestamp, data, noColor = false) => `${chalk.bgKeyword('darkorange')(`[${moment().format(timestamp)}]`)} ${noColor === true ? data : chalk.keyword('darkorange')(data)}`,
   ERROR: (timestamp, data, noColor = false) => `${chalk.bgRedBright(`[${moment().format(timestamp)}]`)} ${noColor === true ? data : chalk.bold.redBright(data)}`,
@@ -49,19 +80,31 @@ export default class Logger {
   }
 
   public info (data: any): this {
-    return this.writeFile('INFO', data)
+    return this.writeFile('INFO', data, {
+      console: this.options.display?.console?.info,
+      file: this.options.display?.file?.info
+    })
   }
 
   public warn (data: any): this {
-    return this.writeFile('WARN', data)
+    return this.writeFile('WARN', data, {
+      console: this.options.display?.console?.warn,
+      file: this.options.display?.file?.warn
+    })
   }
 
   public error (data: any): this {
-    return this.writeFile('ERROR', data)
+    return this.writeFile('ERROR', data, {
+      console: this.options.display?.console?.debug,
+      file: this.options.display?.file?.debug
+    })
   }
 
   public debug (data: any): this {
-    return this.writeFile('DEBUG', data)
+    return this.writeFile('DEBUG', data, {
+      console: this.options.display?.console?.debug,
+      file: this.options.display?.file?.debug
+    })
   }
 
   public setTimestampFormat (timestamp: string): this {
@@ -74,10 +117,9 @@ export default class Logger {
     return this.timestamp
   }
 
-  private writeFile (type: LogType, data: any): this {
-    const isObject = Utils.isObject(data)
-    console.log(colors[type](this.timestamp, Utils.flatten(data, isObject), isObject))
-    if (this.stream) this.stream.write(`[${moment().format(this.timestamp)}] [${type}] ${Utils.flatten(data, false) + EOL || '\n'}`)
+  private writeFile (type: LogType, data: any, options: WriteOptions): this {
+    if (options.console) console.log(colors[type](this.timestamp, Utils.flatten(data), Utils.isObject(data)))
+    if (this.stream && options.file) this.stream.write(`[${moment().format(this.timestamp)}] [${type}] ${Utils.flatten(data, false) + EOL || '\n'}`)
 
     return this
   }
@@ -86,7 +128,10 @@ export default class Logger {
 class Utils {
   public static flatten (data: any, color: boolean = true) {
     if (typeof data === 'string') return data
-    if (typeof data === 'object') return inspect(data, { depth: Number(Array.isArray(data)), colors: color })
+    if (typeof data === 'object') {
+      if (data instanceof Error) return data.stack || data.message
+      return inspect(data, { depth: Number(Array.isArray(data)), colors: color })
+    }
 
     return String(data)
   }
